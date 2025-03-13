334ab6aec083a48a0fe3170e65eb52f737152281
yejin <justlikeyejin@gmail.com>
2025-03-13 17:41:09 +0900

    commit

--- src/main/java/com/basic/myspringboot/security/config/SecurityConfig.java
+++ src/main/java/com/basic/myspringboot/security/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.basic.myspringboot.security.config;
 
+import com.basic.myspringboot.security.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +22,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
@@ -37,7 +39,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         UserDetails admin = User.withUsername("adminboot")
                 .password(encoder.encode("pwd1"))
@@ -48,7 +50,21 @@ public class SecurityConfig {
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    // 커스텀한 UserDetailService를 스프링 빈을 설정
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserInfoUserDetailsService();
     }
 
-
+    //DaoAuthenticationProvider 스프링빈으로 설정
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        //DaoAuthenticationProvider에게 UserDetailService와 passwordEncoder를 설정
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
 }
