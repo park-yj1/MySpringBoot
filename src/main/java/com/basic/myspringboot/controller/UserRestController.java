9c942cd7ce76c6172bb20fe6bea12bb325f4ac97
yejin <justlikeyejin@gmail.com>
2025-03-13 17:35:12 +0900

    commit

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,11 +32,13 @@ public class UserRestController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
    public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping(value = "/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public User getUser(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
@@ -64,6 +67,7 @@ public class UserRestController {
 
         //결과값 : Id = 1 User가 삭제처리 되었습니다
     }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome this endpoint is not secure";
